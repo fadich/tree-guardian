5a20e539a426b0e61fbe68819cd774280b751c4f
Fadi A <royalfadich@gmail.com>
2019-07-17 00:14:08 +0300

    Init project

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tree_guardian',
+    url='',
+    author='Fadi A.',
+    author_email='royalfadich@gmail.com',
+    version='0.1.0',
+    description='Watch the directory tree hash and trigger callback on changes detecting.',
+    packages=find_packages(),
+    zip_safe=False,
+    requires=[
+    ]
+)
